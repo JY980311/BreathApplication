0732b905a75c54e1cb7e5cf932d8d54a7acf8f63
JANG JI YONG <zdy2010@naver.com>
2024-07-19 03:04:03 +0900

    "최초 커밋"

---
+++ app/src/main/java/com/example/breathapplication/ui/theme/Color.kt
@@ -0,0 +1,34 @@
+package com.example.breathapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+/** 여기서 부터 우리가 사용할 Color 부분들! */
+val Primary1 = Color(0xFF5F7CFE)
+val Primary2 = Color(0xFF7489E5)
+
+val Secondary1 = Color(0xFFBDCC4D)
+val Secondary2 = Color(0xFF8EA100)
+
+val Greyscale11 = Color(0xFF111215)
+val Greyscale10 = Color(0xFF1B1D21)
+val Greyscale9 = Color(0xFF1E2127)
+val Greyscale8 = Color(0xFF202227)
+val Greyscale7 = Color(0xFF31353E)
+val Greyscale6 = Color(0xFF363B45)
+val Greyscale5 = Color(0xFF626875)
+val Greyscale4 = Color(0xFF99A3C0)
+val Greyscale3 = Color(0xFFBECFF8)
+val Greyscale2 = Color(0xFFE2E5FF)
+val Greyscale1 = Color(0xFFFFFFFF)
+
+
+
+
