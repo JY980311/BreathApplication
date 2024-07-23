8a95442e1b42405c9b8246dbfe6a05b55011a97f
jiyong <jangjiyong1998@gamil.com>
2024-07-23 11:37:34 +0900

    "Test 추가"

--- app/src/main/java/com/example/breathapplication/network/test/Test.kt
+++ app/src/main/java/com/example/breathapplication/network/test/Test.kt
@@ -1,19 +1,9 @@
 package com.example.breathapplication.network.test
 
-import android.util.Log
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewModelScope
-import com.example.breathapplication.network.model.GetSupabaseDate
-import com.example.breathapplication.network.model.PostSupabaseData
-import com.example.breathapplication.network.retrofit.RetrofitClient
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 
 @Composable
 fun Test(
--- app/src/main/java/com/example/breathapplication/network/test/TestViewModel.kt
+++ app/src/main/java/com/example/breathapplication/network/test/TestViewModel.kt
@@ -3,7 +3,7 @@ package com.example.breathapplication.network.test
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.breathapplication.network.model.GetSupabaseDate
+import com.example.breathapplication.network.model.GetSupabaseData
 import com.example.breathapplication.network.retrofit.RetrofitClient
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -11,8 +11,8 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class TestViewModel: ViewModel() {
-    private val _test = MutableStateFlow<List<GetSupabaseDate>>(emptyList())
-    val test: StateFlow<List<GetSupabaseDate>> = _test.asStateFlow()
+    private val _test = MutableStateFlow<List<GetSupabaseData>>(emptyList())
+    val test: StateFlow<List<GetSupabaseData>> = _test.asStateFlow()
 
     init {
         getAllPostData()
@@ -22,7 +22,7 @@ class TestViewModel: ViewModel() {
         viewModelScope.launch {
             val apiService = RetrofitClient.getSupabaseApiService()
             try {
-                val apiResponse: List<GetSupabaseDate> = apiService.getAllPost()
+                val apiResponse: List<GetSupabaseData> = apiService.getAllPost()
                 _test.value = apiResponse
             } catch (e: Exception) {
                 Log.d("PostViewModel", "error: ${e}")
