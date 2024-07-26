4ceb9fca8051943b799044268c3a30422780e958
ChoiYoonSoo <gews30025@naver.com>
2024-07-27 02:34:58 +0900

    "MainActivity 네비게이션 테스트"

--- app/src/main/java/com/example/breathapplication/MainActivity.kt
+++ app/src/main/java/com/example/breathapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.breathapplication
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,9 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.breathapplication.navigation.NavigationGraph
 import com.example.breathapplication.ui.theme.BreathApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +27,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavigationGraph(navController = navController)
                 }
             }
         }
