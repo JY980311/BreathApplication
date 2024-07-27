371619d5a81e759ac2ef39ab3f5408656e14be3b
jiyong <jangjiyong1998@gamil.com>
2024-07-27 18:45:41 +0900

    "Asleep을 포함해서 TestViewModel 완성"

--- app/src/main/java/com/example/breathapplication/network/test/TestViewModel.kt
+++ app/src/main/java/com/example/breathapplication/network/test/TestViewModel.kt
@@ -3,19 +3,29 @@ package com.example.breathapplication.network.test
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.breathapplication.network.model.GetSupabaseData
+import com.example.breathapplication.network.model.asleep.AsleepData
+import com.example.breathapplication.network.model.asleep.Result
+import com.example.breathapplication.network.model.asleep.Session
+import com.example.breathapplication.network.model.asleep.Stat
+import com.example.breathapplication.network.model.supabase.GetSupabaseData
 import com.example.breathapplication.network.retrofit.RetrofitClient
+import com.example.breathapplication.network.test.TestState.TestState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import kotlin.time.TestTimeSource
 
-class TestViewModel: ViewModel() {
+class TestViewModel : ViewModel() {
     private val _test = MutableStateFlow<List<GetSupabaseData>>(emptyList())
     val test: StateFlow<List<GetSupabaseData>> = _test.asStateFlow()
 
+    private val _asleepData = MutableStateFlow(TestState().asleepDataState)
+
+    val asleepData: StateFlow<AsleepData> = _asleepData.asStateFlow()
+
     init {
-        getAllPostData()
+        getAllAsleepData()
     }
 
     private fun getAllPostData() {
@@ -26,6 +36,18 @@ class TestViewModel: ViewModel() {
                 _test.value = apiResponse
             } catch (e: Exception) {
                 Log.d("PostViewModel", "error: ${e}")
+            }
+        }
+    }
+
+    fun getAllAsleepData() {
+        viewModelScope.launch {
+            val apiService = RetrofitClient.getAsleepApiService()
+            try {
+                val apiResponse: AsleepData = apiService.getAsleepData()
+                _asleepData.value = apiResponse
+            } catch (e: Exception) {
+                Log.d("PostViewModel - Asleep", "error: ${e.message}")
             }
         }
     }
