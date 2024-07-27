93ab165d1218dd09c6b925c2cf363e34ba2db3ca
jiyong <jangjiyong1998@gamil.com>
2024-07-27 18:44:51 +0900

    "Asleep Test 부분 추가"

--- app/src/main/java/com/example/breathapplication/network/test/Test.kt
+++ app/src/main/java/com/example/breathapplication/network/test/Test.kt
@@ -1,7 +1,12 @@
 package com.example.breathapplication.network.test
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
@@ -10,8 +15,16 @@ fun Test(
     testViewModel: TestViewModel
 ) {
     val test = testViewModel.test.collectAsStateWithLifecycle()
+    val asleepData = testViewModel.asleepData.collectAsStateWithLifecycle()
 
-    Text(text = test.value.toString())
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        //Text(text = test.value.toString())
+        Text(text = asleepData.value.toString())
+    }
 }
 
 @Preview(showBackground = true)
