89445a0ddde6dfdc454aa2661c52c58f166d6e7a
jiyong <jangjiyong1998@gamil.com>
2024-07-27 18:43:09 +0900

    "Retrofit 클라이언트 추가"

--- app/src/main/java/com/example/breathapplication/network/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/breathapplication/network/retrofit/RetrofitClient.kt
@@ -11,9 +11,14 @@ class RetrofitClient {
     companion object { // 객체 생성 없이 사용할 수 있도록 함
 
         private const val SUPABASE_URL = "https://ebuvlzafrddkrqjhhvdm.supabase.co/rest/v1/"
+        private const val ASLEEP_URL = "https://api.asleep.ai/"
 
         private var supabaseService: SupabaseApiService? = null // supabaseApiService 인터페이스를 사용할 수 있도록 선언
+        private var asleepApiService: AsleepApiService? = null
 
+        /**
+         * SupabaseApiService 반환하는 메소드!
+         * */
         fun getSupabaseApiService(): SupabaseApiService {
             if (supabaseService == null) {
 
@@ -35,6 +40,34 @@ class RetrofitClient {
                     .create(SupabaseApiService::class.java) // IApiService 인터페이스를 사용할 수 있도록 생성
             }
             return supabaseService!! // apiService가 null이 아닌 경우 반환
+        }
+
+        /**
+         * AsleepApiService 반환하는 메소드
+         * 일단은 1개의 데이터만 받아오게 했습니다!
+         * 데이터가 많으면 다 가져오는게 아닌 특정 데이터 값 1개의 값만 가져옵니다!
+         * */
+        fun getAsleepApiService(): AsleepApiService {
+            if (asleepApiService == null) {
+
+                val okHttpClient = OkHttpClient.Builder()
+                    .connectTimeout(15, TimeUnit.SECONDS)
+                    .readTimeout(15, TimeUnit.SECONDS)
+                    .writeTimeout(15, TimeUnit.SECONDS)
+                    .build()
+
+                val moshi = Moshi.Builder()
+                    .add(KotlinJsonAdapterFactory())
+                    .build()
+
+                asleepApiService = Retrofit.Builder()
+                    .baseUrl(ASLEEP_URL)
+                    .addConverterFactory(MoshiConverterFactory.create(moshi))
+                    .client(okHttpClient)
+                    .build()
+                    .create(AsleepApiService::class.java) // IApiService 인터페이스를 사용할 수 있도록 생성
+            }
+            return asleepApiService!! // apiService가 null이 아닌 경우 반환
         }
     }
 }
