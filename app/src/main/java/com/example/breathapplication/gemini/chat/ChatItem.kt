0b62df1e5d5c13f41ee169f9c5a9a9c09dbfd0ff
JANG JI YONG <zdy2010@naver.com>
2024-07-30 02:44:35 +0900

    "ChatItem 부분 전면 수정 -> 각 메세지에 bot icon 나오게 수정"

--- app/src/main/java/com/example/breathapplication/gemini/chat/ChatItem.kt
+++ app/src/main/java/com/example/breathapplication/gemini/chat/ChatItem.kt
@@ -1,16 +1,21 @@
 package com.example.breathapplication.gemini.chat
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import com.example.breathapplication.gemini.chat.data.ChatData
 import com.example.breathapplication.ui.theme.Greyscale4
@@ -21,30 +26,43 @@ import com.example.breathapplication.ui.theme.Typography2
 fun ChatItem(
     message: ChatData.Message
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(4.dp)
+    Row(
+        modifier = Modifier.fillMaxWidth()
     ) {
-        Box(
-            modifier = Modifier
-                .align(if (message.isFromMe) Alignment.End else Alignment.Start)
-                .clip(
-                    RoundedCornerShape(
-                        topStart = if (message.isFromMe) 12.dp else 2.dp,
-                        topEnd = if (message.isFromMe) 2.dp else 12.dp,
-                        bottomEnd = 12.dp,
-                        bottomStart = 12.dp
-                    )
-                )
-                .background(Greyscale9)
-                .padding(horizontal = 22.dp, vertical = 14.dp)
-        ) {
-            Text(
-                text = message.text,
-                style = Typography2.bodyText,
-                color = Greyscale4
+
+        message.icon?.let {
+            Image(
+                imageVector = ImageVector.vectorResource(id = message.icon),
+                contentDescription = ""
             )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = if(message.icon != null) 16.dp else 64.dp, bottom = 8.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .align(if (message.isFromMe) Alignment.End else Alignment.Start)
+                    .widthIn(max = 300.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            topStart = if (message.isFromMe) 12.dp else 2.dp,
+                            topEnd = if (message.isFromMe) 2.dp else 12.dp,
+                            bottomEnd = 12.dp,
+                            bottomStart = 12.dp
+                        )
+                    )
+                    .background(Greyscale9)
+                    .padding(horizontal = 22.dp, vertical = 14.dp)
+            ) {
+                Text(
+                    text = message.text,
+                    style = Typography2.bodyText,
+                    color = Greyscale4
+                )
+            }
         }
     }
 }
