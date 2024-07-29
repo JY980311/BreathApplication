bf0f351f76a994489ac4882aa7bde8267b5db629
ChoiYoonSoo <gews30025@naver.com>
2024-07-29 17:28:14 +0900

    "수면일기 write 화면 수정"

--- app/src/main/java/com/example/breathapplication/screen/WriteDiaryScreen.kt
+++ app/src/main/java/com/example/breathapplication/screen/WriteDiaryScreen.kt
@@ -1,7 +1,6 @@
 package com.example.breathapplication.screen
 
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -21,8 +20,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,7 +31,6 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.breathapplication.R
 import com.example.breathapplication.component.CompleteButton
@@ -48,22 +44,19 @@ import com.example.breathapplication.ui.theme.Greyscale10
 import com.example.breathapplication.ui.theme.Greyscale2
 import com.example.breathapplication.ui.theme.Greyscale5
 import com.example.breathapplication.ui.theme.Typography2
-import com.example.breathapplication.viewmodel.WriteDiaryScreenViewModel
+import com.example.breathapplication.viewmodel.DiaryScreenViewModel
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun WriteDiaryScreen(navController: NavHostController, writeViewModel: WriteDiaryScreenViewModel = viewModel()) {
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
+fun WriteDiaryScreen(navController: NavHostController, diaryScreenViewModel: DiaryScreenViewModel) {
+    diaryScreenViewModel.setSelectedDate("")
 
-    LaunchedEffect(navBackStackEntry) {
-        Log.d("Tlqkf", "Current route: ${navBackStackEntry?.destination?.route}")
-    }
     Column(
         Modifier
             .background(Color.Black)
     )
     {
-        TobBar(title = writeViewModel.TopbarCrrentDate)
+        TobBar(title = diaryScreenViewModel.TopbarDate.value, R.drawable.ic_calendar, R.drawable.ic_setting)
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
@@ -72,7 +65,7 @@ fun WriteDiaryScreen(navController: NavHostController, writeViewModel: WriteDiar
             item {
                 Column {
                     Text(
-                        text = writeViewModel.currentDate,
+                        text = diaryScreenViewModel.subTitleDate.value,
                         style = Typography2.subTitle,
                         color = Greyscale5,
                         textAlign = TextAlign.Start,
@@ -118,7 +111,7 @@ fun WriteDiaryScreen(navController: NavHostController, writeViewModel: WriteDiar
                 )
 
                 Spacer(modifier = Modifier.height(14.dp))
-                InputDiaryText(writeViewModel)
+                InputDiaryText(diaryScreenViewModel)
 
                 Spacer(modifier = Modifier.height(56.dp))
                 Text(
@@ -128,7 +121,7 @@ fun WriteDiaryScreen(navController: NavHostController, writeViewModel: WriteDiar
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
-                SleepTagList(writeViewModel)
+                SleepTagList(diaryScreenViewModel)
 
                 Spacer(modifier = Modifier.height(56.dp))
                 Text(
@@ -138,26 +131,27 @@ fun WriteDiaryScreen(navController: NavHostController, writeViewModel: WriteDiar
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
-                MoodTagList(writeViewModel)
+                MoodTagList(diaryScreenViewModel)
 
                 Spacer(modifier = Modifier.height(76.dp))
                 CompleteButton("작성 완료", onClick = {
-                    writeViewModel.showDialog = true
-                    Log.d("데이터 확인", "일기 내용: ${writeViewModel.diaryText} 선택한 수면방해 요인: ${writeViewModel.getSelectedTag()} 선택한 기분: ${writeViewModel.selectedTag.value}")
+                    diaryScreenViewModel.writeShowDialog = true
+                    diaryScreenViewModel.setReadDiaryText()
+                    diaryScreenViewModel.setReadSleepTag(diaryScreenViewModel.getSelectedDisturbTag())
                 })
             }
         }
 
-        if (writeViewModel.showDialog) {
-            CompleteDialog1(writeViewModel, navController)
+        if (diaryScreenViewModel.writeShowDialog) {
+            CompleteDialog1(diaryScreenViewModel, navController)
         }
     }
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun CompleteDialog1(writeViewModel: WriteDiaryScreenViewModel, navController : NavHostController){
-    Dialog(onDismissRequest = { writeViewModel.showDialog = false }) {
+fun CompleteDialog1(diaryScreenViewModel: DiaryScreenViewModel, navController : NavHostController){
+    Dialog(onDismissRequest = { diaryScreenViewModel.writeShowDialog = false }) {
         Surface(
             color = Color.White,
             modifier = Modifier
@@ -189,12 +183,12 @@ fun CompleteDialog1(writeViewModel: WriteDiaryScreenViewModel, navController : N
                 NormalButton("일기 먼저 볼래요", onClick = {
                     navController.navigate(
                         ButtonNavItem.ReadDiaryScreen.route)
-                    writeViewModel.showDialog = false
+                    diaryScreenViewModel.writeShowDialog = false
                 })
 
                 Spacer(modifier = Modifier.height(10.dp))
 
-                CompleteButton("수면 측정 하러가기", onClick = { writeViewModel.showDialog = false })
+                CompleteButton("수면 측정 하러가기", onClick = { diaryScreenViewModel.writeShowDialog = false })
 
             }
         }
@@ -203,7 +197,7 @@ fun CompleteDialog1(writeViewModel: WriteDiaryScreenViewModel, navController : N
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun SleepTagList(writeViewModel: WriteDiaryScreenViewModel) {
+fun SleepTagList(diaryScreenViewModel: DiaryScreenViewModel) {
     val tagTextList = listOf("과한 운동", "과음", "카페인")
 
     LazyRow(
@@ -211,8 +205,8 @@ fun SleepTagList(writeViewModel: WriteDiaryScreenViewModel) {
         horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         items(tagTextList.size) { index ->
-            DisturbButton(tag = tagTextList[index], isClicked = writeViewModel.sleepTapClicked[index]) {
-                writeViewModel.sleepTapClicked[index] = !writeViewModel.sleepTapClicked[index]
+            DisturbButton(tag = tagTextList[index], isClicked = diaryScreenViewModel.sleepTapClicked[index]) {
+                diaryScreenViewModel.sleepTapClicked[index] = !diaryScreenViewModel.sleepTapClicked[index]
             }
         }
     }
@@ -220,7 +214,7 @@ fun SleepTagList(writeViewModel: WriteDiaryScreenViewModel) {
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun MoodTagList(writeViewModel: WriteDiaryScreenViewModel) {
+fun MoodTagList(diaryScreenViewModel: DiaryScreenViewModel) {
     val tagTextList = listOf(
         "행복해요", "편안해요", "내일이 기대돼요",
         "잠이 솔솔와요", "그냥 그래요", "슬퍼요",
@@ -229,7 +223,6 @@ fun MoodTagList(writeViewModel: WriteDiaryScreenViewModel) {
 
     val itemsPerRow = 3
     val rows = tagTextList.chunked(itemsPerRow)
-    val selectedTag by writeViewModel.selectedTag
 
     Column(
         modifier = Modifier.fillMaxWidth(),
@@ -242,9 +235,10 @@ fun MoodTagList(writeViewModel: WriteDiaryScreenViewModel) {
             ) {
                 rowItems.forEach { tag ->
                     MoodTag(tag,
-                        isSelected = tag == selectedTag,
+                        isSelected = tag == diaryScreenViewModel.writeMoodTag.value,
                         onClick = {
-                            writeViewModel.selectTag(if (tag == selectedTag) null else tag)
+                            diaryScreenViewModel.selectMoodTag(if (tag == diaryScreenViewModel.writeMoodTag.value) null else tag)
+                            diaryScreenViewModel.setReadMoodTag()
                         }
                     )
                 }
@@ -258,5 +252,5 @@ fun MoodTagList(writeViewModel: WriteDiaryScreenViewModel) {
 @Composable
 fun WriteDiaryScreenPreview() {
     val navController = rememberNavController()
-    WriteDiaryScreen(navController)
+    WriteDiaryScreen(navController, viewModel())
 }
