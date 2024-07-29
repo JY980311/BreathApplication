c76a9ce0cf3f3960cc55a8c12528f41e143fcfe1
JANG JI YONG <zdy2010@naver.com>
2024-07-30 02:47:26 +0900

    "Questions object 추가"

---
+++ app/src/main/java/com/example/breathapplication/gemini/chat/data/Questions.kt
@@ -0,0 +1,21 @@
+package com.example.breathapplication.gemini.chat.data
+
+object Questions {
+    val questions = listOf(
+        QuestionsData(1, "평균적으로 하루 몇 시간 잠을 주무세요?"),
+        QuestionsData(2, "주로 몇 시에 잠자리에 들고 몇 시에 일어나세요?"),
+        QuestionsData(3, "주말에는 평균적으로 하루 몇 시간 잠을 주무세요?"),
+        QuestionsData(4, "낮잠을 자는 경우, 하루 평균 몇 시간 낮잠을 주무세요?"),
+        QuestionsData(5, "잠들기까지 평균적으로 얼마나 걸리세요?"),
+        QuestionsData(6, "밤중에 잠에서 깨는 경우가 잦으신가요?"),
+        QuestionsData(7, "아침에 일어났을 때 개운한 느낌이 드시나요?"),
+        QuestionsData(8, "낮 동안 졸음이 자주 오나요?"),
+        QuestionsData(9, "최근 한 달 동안 수면 패턴에 변화가 있었나요? 있다면 어떤 변화가 있었는지 간단히 설명해주세요."),
+        QuestionsData(10,"잠자리에 들기 전 카페인이나 알코올을 섭취하시나요?"),
+        QuestionsData(11,"운동을 하시나요? 그렇다면 주당 몇 번, 얼마나 운동하시나요?"),
+        QuestionsData(12,"스트레스를 많이 받으시는 편입니까? 그렇다면 어떤 종류의 스트레스를 받으시나요?"),
+        QuestionsData(13, "만성 질환이 있으신가요? 그렇다면 어떤 질환이 있으신가요?"),
+        QuestionsData(14, "최근 1개월 동안 약을 복용하셨나요? 그렇다면 어떤 약을 복용하셨나요?"),
+        QuestionsData(15, "정신 건강 문제로 진단을 받은 적이 있으신가요? 그렇다면 어떤 문제로 진단을 받으셨나요?")
+    )
+}
