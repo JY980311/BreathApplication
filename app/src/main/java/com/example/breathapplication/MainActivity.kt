d8fe3e0e056dfb1522885f13d6137cb525388fb2
ChoiYoonSoo <gews30025@naver.com>
2024-08-01 01:43:47 +0900

    "네비게이션 테스트 코드 추가"

--- app/src/main/java/com/example/breathapplication/MainActivity.kt
+++ app/src/main/java/com/example/breathapplication/MainActivity.kt
@@ -5,25 +5,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowCompat
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
-import com.example.breathapplication.gemini.chat.ChatScreen
-import com.example.breathapplication.gemini.chat.ChatViewModel
-import com.example.breathapplication.gemini.chat.data.ChatData
-import com.example.breathapplication.navigation.NavigationGraph
-import com.example.breathapplication.settingnavigation.SettingNavGraph
+import com.example.breathapplication.navigation.RootNavGraph
 import com.example.breathapplication.ui.theme.BreathApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,9 +29,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SettingNavGraph()
+                    TestApp()
                 }
             }
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @Composable
+    fun TestApp(){
+        val navController = rememberNavController()
+        RootNavGraph(navController = navController)
     }
 }
