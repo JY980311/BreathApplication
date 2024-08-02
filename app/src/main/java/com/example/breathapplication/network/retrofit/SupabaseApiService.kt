7e9b9df9e2aa9fed69c794b3ed68f0b976bd357d
ChoiYoonSoo <gews30025@naver.com>
2024-08-02 22:56:00 +0900

    "수면일기 API 통신 코드 추가"

--- app/src/main/java/com/example/breathapplication/network/retrofit/SupabaseApiService.kt
+++ app/src/main/java/com/example/breathapplication/network/retrofit/SupabaseApiService.kt
@@ -7,10 +7,20 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface SupabaseApiService : AsleepApiService {
+
+    /** id에 맞는 데이터 받아오기 */
+    @GET("Post")
+    suspend fun getPostById(
+        @Query("id") id: String,
+        @Query("select") select: String = "*",
+        @Query("apikey") apikey: String = BuildConfig.SUPABASE_API_KEY,
+    ): List<GetSupabaseData>
+
 
     /** 데이터 받아오기 */
     @GET("Post")
@@ -31,4 +41,18 @@ interface SupabaseApiService : AsleepApiService {
         @Query("apikey") apikey: String = BuildConfig.SUPABASE_API_KEY,
         @Body postSupabaseData: PostSupabaseData
     ) : Response<List<PostSupabaseData>>
+
+    /** 같은 id가 있을 경우 데이터 업데이트 */
+    @Headers(
+        "apikey: ${BuildConfig.SUPABASE_API_KEY}",
+        "Authorization: Bearer ${BuildConfig.SUPABASE_API_KEY}",
+        "Content-Type: application/json",
+        "Prefer: return=representation"
+    )
+    @PATCH("Post")
+    suspend fun updatePost(
+        @Query("id") id: String,
+        @Query("apikey") apikey: String = BuildConfig.SUPABASE_API_KEY,
+        @Body postSupabaseData: PostSupabaseData
+    ): Response<List<PostSupabaseData>>
 }
